bd89342feaf5b851ed0be2ed890eb06e28d62b55
MihailJosifovski <josifovski.mihail@uklo.edu.mk>
2022-12-04 20:47:06 +0100

    push commit

---
+++ src/zadacha9/ClassMain.java
@@ -0,0 +1,12 @@
+package zadacha9;
+
+public class ClassMain {
+	public static void main(String[] args) {
+		Fakultet obj1 = new Fakultet("FIKT", 2, 100);
+		Fakultet obj2 = new Fakultet("TFB", 4, 150);
+		Fakultet obj3 = new Fakultet("FINKI", 5, 350);
+		Fakultet obj4 = new Fakultet("Bioloshki", 2, 150);
+		Fakultet obj5 = new Fakultet("Medicinski", 4, 400);
+		
+	}
+}
---
+++ src/zadacha9/Fakultet.java
@@ -0,0 +1,13 @@
+package zadacha9;
+
+public class Fakultet {
+	private String ime;
+	private int brNaSmerovi;
+	private int brNaStudenti;
+	Fakultet (String Ime, int BrNaSmerovi, int BrNaStudenti){
+		this.ime = Ime;
+		this.brNaSmerovi = BrNaSmerovi;
+		this.brNaStudenti = BrNaStudenti;
+	}
+	
+}
